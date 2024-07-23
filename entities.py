4a26dac332dbe05691e98cb2e7167bfbfe150fff
marinasemak <marina.semak@gmail.com>
2024-07-23 19:10:58 +0300

    add ValueError

--- entities.py
+++ entities.py
@@ -1,5 +1,5 @@
 class Field:
-    def __init__(self, value):
+    def __init__(self, value: str):
         self.value = value
 
     def __str__(self):
@@ -7,15 +7,23 @@ class Field:
 
 
 class Name(Field):
+    def __init__(self, value: str):
+        super().__init__(value)
+        print(f"asasasa  {len(self.value)}")
+        self.validate_name()
 
     def validate_name(self):
         if len(self.value) < 1:
-            print("error")
+            raise ValueError("Name can't be empty")
 
 
 class Phone(Field):
+    def __init__(self, value: str):
+        super().__init__(value)
+        print(f"asasasa  {len(self.value)}")
+        self.validate_phone()
 
-    def validate_name(self):
+    def validate_phone(self):
         print(len(self.value))
         if len(self.value) != 10:
             raise ValueError("Phone number must be 10 digits long")
